75339a095d264727758cef97ea750ee950215bc2
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-22 17:59:03 -0600

    Add capitalize tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,21 @@
 import capitalize from "../scripts/capitalize.js";
 
 describe("capitalize tests", () => {
-  test("return Erick", () => {
+  test("Return Erick", () => {
     expect(capitalize("erick")).toBe("Erick");
+  });
+
+  test("Already capitalized strings will not change", () => {
+    expect(capitalize("Armorer")).toBe("Armorer");
+  });
+
+  test("String in uppercase will not change", () => {
+    expect(capitalize("SELECT")).toBe("SELECT");
+  });
+
+  test("Only capitalize first word for long strings", () => {
+    expect(capitalize("this is my beautiful string")).toBe(
+      "This is my beautiful string"
+    );
   });
 });
