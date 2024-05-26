ee8a0fe8f09c9fe57f4e11cb0f2667cf7dc4b7e7
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-25 22:45:15 -0600

    refactor: Separating caesarCipher core tasks into small functions

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,9 +1,16 @@
+// If upper case, use "A" as reference of counting the index, otherwise use "a"
+const getRefIndex = (char) => (char.toUpperCase() === char ? 65 : 97);
+
+// Shift character based on their ASCII and reference index
+// then wrap characters if necessary (z -> a)
+const charToIndex = (char, reference, shift, wrapFactor) =>
+  (char.charCodeAt(0) - reference + shift) % wrapFactor;
+
 const caesarCipher = (text, shift = 0) => {
   // Replace only letters (avoid numbers and special characters)
   return text.replace(/[a-zA-z]/g, (char) => {
-    const isUpper = char.toUpperCase() === char;
-    const reference = isUpper ? 65 : 97; // If upper case, use "A" as reference of counting the index, otherwise use "a"
-    const index = (char.charCodeAt(0) - reference + shift) % 26; // Wrap characters (z -> a)
+    const reference = getRefIndex(char);
+    const index = charToIndex(char, reference, shift, 26);
     return String.fromCharCode(index + reference); // Convert ASCII to character
   });
 };
