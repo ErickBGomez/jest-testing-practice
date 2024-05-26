d261afcbfb0574b213cc6c8cc005b7f18ca9378b
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-26 09:40:00 -0600

    feat: Define every function for analyzeArray script

--- scripts/analyzeArray.js
+++ scripts/analyzeArray.js
@@ -1,5 +1,19 @@
+const getMin = (array) => Math.min(...array);
+
+const getMax = (array) => Math.max(...array);
+
+// Sum all elements and divide by array length
+const getAverage = (array) => array.reduce((a, num) => a + num) / array.length;
+
+const getLength = (array) => array.length;
+
 const analyzeArray = (array) => {
-  return { average: 4, min: 1, max: 8, length: 6 };
+  return {
+    average: getAverage(array),
+    min: getMin(array),
+    max: getMax(array),
+    length: getLength(array),
+  };
 };
 
 export default analyzeArray;
