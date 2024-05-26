8631fdbcccc7fea3c268f36f8193a4cb1a0ae730
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-25 21:22:04 -0600

    Cleaning code and adding comments

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,8 +1,8 @@
 const caesarCipher = (text, shift) => {
+  // Convert string to array of ASCII codes, then add shift value to each character
   const map = text.split("").map((char) => char.charCodeAt(0) + shift);
-  const shifted = String.fromCharCode(...map);
-
-  return shifted;
+  // Convert ASCII array to string
+  return String.fromCharCode(...map);
 };
 
 // console.log(String.fromCharCode("a".charCodeAt(0) + 3));
