d38f95c0fa4577e47d426ae2253216ecf85ad6ce
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-26 09:48:49 -0600

    Add more tests

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -9,4 +9,40 @@ describe("Analyze array tests", () => {
       length: 6,
     });
   });
+
+  test("analyzing 2", () => {
+    expect(analyzeArray([0, 5, 7, 10, 4, 1, 3, 2])).toStrictEqual({
+      average: 4,
+      min: 0,
+      max: 10,
+      length: 8,
+    });
+  });
+
+  test("Same values", () => {
+    expect(analyzeArray([2, 2, 2, 2, 2])).toStrictEqual({
+      average: 2,
+      min: 2,
+      max: 2,
+      length: 5,
+    });
+  });
+
+  test("Consecutive", () => {
+    expect(analyzeArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])).toStrictEqual({
+      average: 4.5,
+      min: 0,
+      max: 9,
+      length: 10,
+    });
+  });
+
+  test("All negatives", () => {
+    expect(analyzeArray([-10, -11, -22, -5, -4, -8])).toStrictEqual({
+      average: -10,
+      min: -22,
+      max: -4,
+      length: 6,
+    });
+  });
 });
