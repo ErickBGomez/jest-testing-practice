b2f6f9dc4e4c0afb2f214e2434ff26322b743243
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-05-26 09:24:46 -0600

    Start analyzeArray tests

---
+++ scripts/analyzeArray.js
@@ -0,0 +1,5 @@
+const analyzeArray = (array) => {
+  return { average: 4, min: 1, max: 8, length: 6 };
+};
+
+export default analyzeArray;
---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,12 @@
+import analyzeArray from "../scripts/analyzeArray";
+
+describe("Analyze array tests", () => {
+  test("analyzing 1", () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    });
+  });
+});
